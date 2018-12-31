805e55991da93bfa3521edf13f454fd394605db3
georges <georges.georgoulis@wanadoo.fr>
2018-12-31 23:12:23 +0100

    set debug=False in prod

--- ec1/settings.py
+++ ec1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','zmrjos(f)q^24_c4x8zfj$a#kr)24!@+pqc22@
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = True
+    DEBUG = False
     ALLOWED_HOSTS = ['serene-brushlands-40275.herokuapp.com']
 else:
     DEBUG = True
