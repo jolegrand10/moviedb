42419b87913b5e0cdda105496750e173ba8899c5
georges <georges.georgoulis@wanadoo.fr>
2018-12-31 22:09:12 +0100

    Attempt to fix static file import bug in settings.py

--- ec1/settings.py
+++ ec1/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 if os.environ.get('ENV') == 'PRODUCTION' :
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    PROJECT_ROOT= os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
