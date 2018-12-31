73099b70b3c230eca17429fb9f61528120a518e4
georges <georges.georgoulis@wanadoo.fr>
2018-12-31 19:41:55 +0100

    Changed from sqlite3 to Postgresql

--- ec1/settings.py
+++ ec1/settings.py
@@ -29,7 +29,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['ec1.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ec1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviedb',
+        'USER': 'dbadmin',
+        'PASSWORD' : 'dbadmin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
