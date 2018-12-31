d3ab6d88e0c4d49e2d81a839d71b5112ccf84c9c
georges <georges.georgoulis@wanadoo.fr>
2018-12-31 22:12:56 +0100

    New attempt to fix static file import bug in settings.py

--- ec1/settings.py
+++ ec1/settings.py
@@ -130,7 +130,7 @@ if os.environ.get('ENV') == 'PRODUCTION' :
     PROJECT_ROOT= os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
+        os.path.join(PROJECT_ROOT, 'static'),
     )
     # simplified static file serving
     # https://warehouse.python.org/project/whitenoise/
