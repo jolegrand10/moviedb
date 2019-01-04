4c0e1f701fe1f6de01bb40a2357603ac20239cac
georges <georges.georgoulis@wanadoo.fr>
2019-01-04 20:46:12 +0100

    Restored nominal settings in settings.py section static files in prod

--- ec1/settings.py
+++ ec1/settings.py
@@ -28,7 +28,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = False
     ALLOWED_HOSTS = ['localhost','serene-brushlands-40275.herokuapp.com']
 else:
-    DEBUG = False
+    DEBUG = True
     ALLOWED_HOSTS = ['localhost']
 
 
@@ -60,8 +60,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ec1.urls'
 
-
-INTERNAL_IPS = ['127.0.0.1']
 
 TEMPLATES = [
     {
@@ -132,18 +130,34 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_URL = '/static/'
+
+
+#Django debug toolbar
+INTERNAL_IPS = ['127.0.0.1']
+
 if os.environ.get('ENV') == 'PRODUCTION' :
+    #Static files settings
     PROJECT_ROOT= os.path.dirname(os.path.abspath(__file__))
+    print("BASE_DIR=", BASE_DIR)
+    print("PROJECT_ROOT=", PROJECT_ROOT)
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+    print("STATIC_ROOT=", STATIC_ROOT)
+
+    #Extra places for collectstatic to find static files
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
+    print("STATICFILES_DIRS=", STATICFILES_DIRS)
+
+    
     # simplified static file serving
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
     
-STATIC_URL = '/static/'
+
 
 
